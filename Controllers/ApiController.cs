f8141202045ed5491a278385b34b8904b7fc4562
prakharmipl <prakhar.s@mishainfotech.com>
2026-04-22 11:59:20 +0530

    Add project files.

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+
+[ApiController]
+[Route("test/[action]")]
+public class ApiController: ControllerBase
+{
+    //[Route("/get")]
+    public string Get()
+    {
+        return "Hellw from controller";
+    }
+
+    //[Route("/get2")]
+    public string Get2()
+    {
+        return "Hellw from controller2";
+    }
+
+    //[Route("/notget")]
+    public string Notget()
+    {
+        return "Hellw from controller2";
+    }
+}
